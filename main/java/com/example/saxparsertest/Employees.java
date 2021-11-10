7af03be90d586fb51a08892d4c2968b235b1c3e5
JVACV <93898460+JVACV@users.noreply.github.com>
2021-11-10 10:01:01 +0530

    Add files via upload

---
+++ main/java/com/example/saxparsertest/Employees.java
@@ -0,0 +1,15 @@
+package com.example.saxparsertest;
+
+import java.util.ArrayList;
+
+public class Employees {
+    private ArrayList<Employee> list;
+
+    public ArrayList<Employee> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Employee> list) {
+        this.list = list;
+    }
+}
